ad291c76736b19eba65a5cba08bb3449f532d5b4
Hoang Duc <mhoangduc25@gmail.com>
2024-01-12 10:23:26 +0700

    solution 1704

--- easy/[1704]halvesAreAlike.js
+++ easy/[1704]halvesAreAlike.js
@@ -7,7 +7,17 @@ var halvesAreAlike = function(s) {
         return false;
     }
     s = s.toLowerCase();
-    
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == 'a' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'e') {
+            if (i < s.length / 2) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+    }
+    return count == 0;
 };
 
 console.log(halvesAreAlike("book"))
