086d36ef39378d6732ae11df117f53b939345f21
Hoang Duc <mhoangduc25@gmail.com>
2024-01-21 11:02:34 +0700

    solution 198

---
+++ medium/[198]rob.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    if (nums.length == 1) return nums[0];
+    const res = new Array(nums.length + 1).fill(0);
+    res[1] = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        res[i+1] = Math.max();
+    }
+};
+
+console.log(rob([1, 2, 3, 1]));
